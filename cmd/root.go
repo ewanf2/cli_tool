3098b54a547f9cac1c87f18fac59b42d925f63a1
Ewan Fraser <ewan.fraser@mastercard.com>
2026-03-08 12:08:56 +0000

    Restructure

--- cmd/root.go
+++ cmd/root.go
@@ -6,27 +6,51 @@ package cmd
 
 import (
 	"os"
-
+	"fmt"
 	"github.com/spf13/cobra"
+	"strconv"
 )
 
 
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "cli_tool",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "greeter CLI",
+	Short: "CLI that does stuff",
+	Long: `CLI that does a lot of stuff For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) { 
+		fmt.Println("Welcome to my CLI. hey lol")
+	},
+}
+var greetCMD = &cobra.Command{
+	Use: "greet",
+	Short: "Greet the user innit",
+	Long: "Function that greets the user i guess",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello,", args[0])
+	},
 }
 
+var quickmaths = &cobra.Command{
+	Use: "quickmaffs",
+	Short: "maths innit",
+	Long: "maths innit bruv",
+	Args: cobra.ExactArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		x, err := strconv.Atoi(args[0])
+		if err != nil { return fmt.Errorf("invalid x: %w", err)}
+		y, err2 := strconv.Atoi(args[1])
+		if err2 != nil { return fmt.Errorf("invalid y: %w", err2)}
+		fmt.Println(x+y)
+		return nil
+	},
+}
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
@@ -40,9 +64,9 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-
+	rootCmd.AddCommand(greetCMD)
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cli_tool.yaml)")
-
+	rootCmd.AddCommand(quickmaths)
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
